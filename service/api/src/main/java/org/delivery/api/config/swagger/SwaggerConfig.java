f2d23890b4352b53c8e9c0fddbef34b83478d0c1
IMWoo94 <leemeawoo94@gmail.com>
2024-01-25 17:33:31 +0900

    feat : 자동화 API 생성 swagger 적용
    1. api 모듈에 swagger 의존성 추가
    2. 스프링부트3.0 버전에서는 기존 ObjectMapper 커스텀으로 자동적용
    미 적용 시 별도의 config 파일로 적용 시켜주면 됨

---
+++ service/api/src/main/java/org/delivery/api/config/swagger/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package org.delivery.api.config.swagger;
+
+// @Configuration
+public class SwaggerConfig {
+
+	// @Bean
+	// public ModelResolver modelResolver(ObjectMapper objectMapper) {
+	// 	return new ModelResolver(objectMapper);
+	// }
+}
