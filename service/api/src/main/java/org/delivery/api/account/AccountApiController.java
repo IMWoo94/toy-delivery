4dc6a4d3cf40bcd473eac34e8892b22acc2d26f4
IMWoo94 <leemeawoo94@gmail.com>
2024-01-29 17:54:11 +0900

    feat : API 공통 스펙 정의
    1. 응답 결과를 담은 Result 부분과 내용을 담을 body 부분으로 나누어서 API 스펙 정의

--- service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,6 +3,7 @@ package org.delivery.api.account;
 import java.time.LocalDateTime;
 
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,12 @@ public class AccountApiController {
 	private final AccountRepository accountRepository;
 
 	@GetMapping("/me")
-	public AccountMeResponse me() {
-		return AccountMeResponse.builder()
+	public Api<AccountMeResponse> me() {
+		var response = AccountMeResponse.builder()
 			.name("홍길동")
 			.email("A@gmail.com")
 			.registeredAt(LocalDateTime.now())
 			.build();
+		return Api.ok(response);
 	}
 }
