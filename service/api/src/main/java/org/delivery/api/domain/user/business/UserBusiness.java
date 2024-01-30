9bd54b259293d9e50619bf833544fed32add713d
IMWoo94 <leemeawoo94@gmail.com>
2024-01-30 21:14:31 +0900

    feat : 사용자 회원가입 로직 구현
    1. UserOpenApiController.register() 에서 회원가입 진행 - 회원 가입은 인증 절차가 필요 없이 모든 사용자가 접근이 가능 해야 함
    2. 회원가입 요청 시 전달되는 UserRegisterRequest 생성 하여 필요 정보 전달
    3. UserBusiness.register() 회원 가입 처리
    4. UserConverter.toEntity() request -> entity 변환
    5. UserService.register() UserEntity 저장
    6. UserConverter.toResponse() new Entity -> response 변환

--- service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -1,6 +1,12 @@
 package org.delivery.api.domain.user.business;
 
+import java.util.Optional;
+
 import org.delivery.api.common.annotation.Business;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
 import org.delivery.api.domain.user.service.UserService;
 
@@ -12,4 +18,17 @@ public class UserBusiness {
 
 	private final UserService userService;
 	private final UserConverter userConverter;
+
+	public UserResponse register(UserRegisterRequest request) {
+		// var entity = userConverter.toEntity(request);
+		// var newEntity = userService.register(entity);
+		// var response = userConverter.toResponse(newEntity);
+
+		return Optional.ofNullable(request)
+			.map(userConverter::toEntity)
+			.map(userService::register)
+			.map(userConverter::toResponse)
+			.orElseThrow(
+				() -> new ApiException(ErrorCode.NULL_POINT, "UserBusiness.register : UserRegisterRequest Null"));
+	}
 }
--- service/api/src/main/java/org/delivery/api/domain/user/converter/UserConverter.java
+++ service/api/src/main/java/org/delivery/api/domain/user/converter/UserConverter.java
@@ -1,10 +1,54 @@
 package org.delivery.api.domain.user.converter;
 
+import java.util.Optional;
+
 import org.delivery.api.common.annotation.Converter;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.delivery.db.user.UserEntity;
 
 import lombok.RequiredArgsConstructor;
 
 @Converter
 @RequiredArgsConstructor
 public class UserConverter {
+
+	public UserEntity toEntity(UserRegisterRequest userRegisterRequest) {
+
+		return Optional.ofNullable(userRegisterRequest)
+			.map(it -> {
+				// to Entity
+
+				return UserEntity.builder()
+					.name(it.getName())
+					.email(it.getEmail())
+					.address(it.getAddress())
+					.password(it.getPassword())
+					.build();
+			})
+			.orElseThrow(
+				() -> new ApiException(ErrorCode.NULL_POINT, "UserConverter.toEntity : UserRegisterRequest Null"));
+	}
+
+	public UserResponse toResponse(UserEntity userEntity) {
+		return Optional.ofNullable(userEntity)
+			.map(it -> {
+				// to UserResponse
+
+				return UserResponse.builder()
+					.id(userEntity.getId())
+					.name(userEntity.getName())
+					.status(userEntity.getStatus())
+					.email(userEntity.getEmail())
+					.address(userEntity.getAddress())
+					.registeredAt(userEntity.getRegisteredAt())
+					.unregisteredAt(userEntity.getUnregisteredAt())
+					.lastLoginAt(userEntity.getLastLoginAt())
+					.build();
+
+			})
+			.orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "UserConverter.toResponse : UserEntity Null"));
+	}
 }
