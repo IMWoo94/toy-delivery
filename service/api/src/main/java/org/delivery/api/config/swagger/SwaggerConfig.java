9b0f066880981afe30743414187ecce2dd09978e
IMWoo94 <leemeawoo94@gmail.com>
2024-02-07 15:32:02 +0900

    fix : swagger ui Naming option modify
    1. Swagger 의 파라미터 Naming 방식을 Camel-case 가 아닌 Snake-case 로 설정

--- service/api/src/main/java/org/delivery/api/config/swagger/SwaggerConfig.java
+++ service/api/src/main/java/org/delivery/api/config/swagger/SwaggerConfig.java
@@ -1,10 +1,17 @@
 package org.delivery.api.config.swagger;
 
-// @Configuration
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.core.jackson.ModelResolver;
+
+@Configuration
 public class SwaggerConfig {
 
-	// @Bean
-	// public ModelResolver modelResolver(ObjectMapper objectMapper) {
-	// 	return new ModelResolver(objectMapper);
-	// }
+	@Bean
+	public ModelResolver modelResolver(ObjectMapper objectMapper) {
+		return new ModelResolver(objectMapper);
+	}
 }
