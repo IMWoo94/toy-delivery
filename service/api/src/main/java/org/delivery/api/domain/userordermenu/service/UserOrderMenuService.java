61a4f1475f9d1efd3aa8180b298edfc8b69b6e08
IMWoo94 <leemeawoo94@gmail.com>
2024-02-08 16:02:48 +0900

    refactor : 주문 메뉴 내역 조회
    1. 주문 ID 를 기반으로 주문 내역 조회 Service 생성
    2. 조회 메서드 생성

---
+++ service/api/src/main/java/org/delivery/api/domain/userordermenu/service/UserOrderMenuService.java
@@ -0,0 +1,21 @@
+package org.delivery.api.domain.userordermenu.service;
+
+import java.util.List;
+
+import org.delivery.db.userordermenu.UserOrderMenuEntity;
+import org.delivery.db.userordermenu.UserOrderMenuRepository;
+import org.delivery.db.userordermenu.enums.UserOrderMenuStatus;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserOrderMenuService {
+
+	private final UserOrderMenuRepository userOrderMenuRepository;
+
+	public List<UserOrderMenuEntity> getUserOrderMenu(Long userOrderId) {
+		return userOrderMenuRepository.findAllByUserOrderIdAndStatus(userOrderId, UserOrderMenuStatus.REGISTERED);
+	}
+}
--- service/db/src/main/java/org/delivery/db/userordermenu/UserOrderMenuRepository.java
+++ service/db/src/main/java/org/delivery/db/userordermenu/UserOrderMenuRepository.java
@@ -1,6 +1,11 @@
 package org.delivery.db.userordermenu;
 
+import java.util.List;
+
+import org.delivery.db.userordermenu.enums.UserOrderMenuStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserOrderMenuRepository extends JpaRepository<UserOrderMenuEntity, Long> {
+
+	List<UserOrderMenuEntity> findAllByUserOrderIdAndStatus(Long userOrderId, UserOrderMenuStatus status);
 }
