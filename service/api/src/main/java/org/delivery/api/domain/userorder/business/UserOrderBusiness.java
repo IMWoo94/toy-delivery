190f44e3694c0defe68b5654fc82d127a686e5fc
IMWoo94 <leemeawoo94@gmail.com>
2024-02-22 19:01:48 +0900

    feat : API Server Producer 설정
    1. commom 모듈 등록
    2. 사용자 주문 시 UserOrderProducer 를 통해서 RabbitMQ 전송

--- service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
+++ service/api/src/main/java/org/delivery/api/domain/userorder/business/UserOrderBusiness.java
@@ -15,6 +15,7 @@ import org.delivery.api.domain.userorder.controller.model.UserOrderDetailRespons
 import org.delivery.api.domain.userorder.controller.model.UserOrderRequest;
 import org.delivery.api.domain.userorder.controller.model.UserOrderResponse;
 import org.delivery.api.domain.userorder.converter.UserOrderConverter;
+import org.delivery.api.domain.userorder.producer.UserOrderProducer;
 import org.delivery.api.domain.userorder.service.UserOrderService;
 import org.delivery.api.domain.userordermenu.converter.UserOrderMenuConverter;
 import org.delivery.api.domain.userordermenu.service.UserOrderMenuService;
@@ -34,6 +35,7 @@ public class UserOrderBusiness {
 	private final UserOrderMenuService userOrderMenuService;
 	private final StoreService storeService;
 	private final StoreConverter storeConverter;
+	private final UserOrderProducer userOrderProducer;
 
 	/**
 	 * 1. 사용자, 메뉴 ID
@@ -75,6 +77,9 @@ public class UserOrderBusiness {
 
 		// 맵핑 등록
 		userOrderMenuEntityList.forEach(userOrderMenuService::order);
+
+		// 비동기로 가맹점에 주문 알리기
+		userOrderProducer.sendOrder(newUserOrderEntity);
 
 		// 응답 결과 반환
 		return userOrderConverter.toResponse(newUserOrderEntity);
---
+++ service/api/src/main/java/org/delivery/api/domain/userorder/producer/UserOrderProducer.java
@@ -0,0 +1,29 @@
+package org.delivery.api.domain.userorder.producer;
+
+import org.delivery.api.common.rabbitmq.Producer;
+import org.delivery.common.message.model.UserOrderMessage;
+import org.delivery.db.userorder.UserOrderEntity;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserOrderProducer {
+
+	private final Producer producer;
+	private static final String EXCHANGE = "delivery.exchange";
+	private static final String ROUTE_KEY = "delivery.key";
+
+	public void sendOrder(UserOrderEntity userOrderEntity) {
+		sendOrder(userOrderEntity.getUserId());
+	}
+
+	public void sendOrder(Long userOrderId) {
+		var message = UserOrderMessage.builder()
+			.userOrderId(userOrderId)
+			.build();
+
+		producer.producer(EXCHANGE, ROUTE_KEY, message);
+	}
+}
