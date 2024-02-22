42eacb619ffcc12f76cca69c9f770fbff84cdf52
IMWoo94 <leemeawoo94@gmail.com>
2024-02-22 16:24:12 +0900

    feat : RabbitMQ 설정
    1. rabbitMQ 를 사용하기 위한 RabbitTemplate 설정
    2. rabbitMQ 에 연결 정보를 Application 에 작성
    3. rabbitMQ message 전송, 수신 시 JSON converter 등록

--- service/api/src/main/java/org/delivery/api/config/rabbitmq/rabbitMqConfig.java
+++ service/api/src/main/java/org/delivery/api/config/rabbitmq/rabbitMqConfig.java
@@ -4,8 +4,14 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class rabbitMqConfig {
@@ -23,5 +29,27 @@ public class rabbitMqConfig {
 	@Bean
 	public Binding binding(DirectExchange directExchange, Queue queue) {
 		return BindingBuilder.bind(queue).to(directExchange).with("delivery.key");
+	}
+
+	// end queue 설정
+
+	/**
+	 * producer -> exchange 로 데이터를 보내는 과정에서 AMQP 프로토콜을 사용하여 전달한다.
+	 * 이렇게 특정 프로토콜을 사용하게 되면서 정해진 규약에 맞춰어 데이터를 전송해야 하는데,
+	 * RestTemplate 과 같은 Template 를 지정 하여 사용 한다.
+	 */
+	@Bean
+	public RabbitTemplate rabbitTemplate(
+		ConnectionFactory connectionFactory,
+		MessageConverter messageConverter
+	) {
+		var rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(messageConverter);
+		return rabbitTemplate;
+	}
+
+	@Bean
+	public MessageConverter messageConverter(ObjectMapper objectMapper) {
+		return new Jackson2JsonMessageConverter(objectMapper);
 	}
 }
